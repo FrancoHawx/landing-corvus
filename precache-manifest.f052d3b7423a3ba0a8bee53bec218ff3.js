af3c4bf25b04099b63fa2b44cbeec9604bc11186
Tipo <fschillage@gmail.com>
2019-06-03 05:12:44 -0300

    Updates

---
+++ precache-manifest.f052d3b7423a3ba0a8bee53bec218ff3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8740403651f5a645cca5e34f18ab8f",
+    "url": "/landing-corvus/index.html"
+  },
+  {
+    "revision": "990940fe265b27416cb9",
+    "url": "/landing-corvus/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "d3db1a867e03e2c08eda",
+    "url": "/landing-corvus/static/css/main.4a86043f.chunk.css"
+  },
+  {
+    "revision": "990940fe265b27416cb9",
+    "url": "/landing-corvus/static/js/2.93fc63b6.chunk.js"
+  },
+  {
+    "revision": "d3db1a867e03e2c08eda",
+    "url": "/landing-corvus/static/js/main.53076112.chunk.js"
+  },
+  {
+    "revision": "dd1f342fc0f95ccfa6da",
+    "url": "/landing-corvus/static/js/runtime~main.40a8c60c.js"
+  },
+  {
+    "revision": "b214c891eb069f2f518c63b9037feb99",
+    "url": "/landing-corvus/static/media/IMG_resp.b214c891.png"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/landing-corvus/static/media/fondo-integrantes.86377fca.png"
+  },
+  {
+    "revision": "1bc66cf19164b153036f09545f183666",
+    "url": "/landing-corvus/static/media/programming_0.1bc66cf1.jpg"
+  },
+  {
+    "revision": "bf78a4c722af8668059b09dc0fb01fb0",
+    "url": "/landing-corvus/static/media/work-731198_1920.bf78a4c7.jpg"
+  }
+]);
